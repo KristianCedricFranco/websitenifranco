11369e96e38d12639d974866263f89b12c99baf1
oOZeroXx <104554822+oOZeroXx@users.noreply.github.com>
2022-04-28 15:20:09 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,113 @@
+<html>
+<head>
+<title>Franco's Website</title>
+</head>
+<style>
+body {
+  background-image: url('lir0qgda.gif');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size:cover ;
+}
+
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 50%;
+}
+
+td, th {
+  border: 1px solid #BEBEBE;
+  text-align: left;
+  padding: 8px;
+  width: 50%;
+}
+
+tr:nth-child(even) {
+  background-color: #808080;
+  width: 50%;
+}
+</style>
+<br>
+<h1 style="color:yellow;font-size: 300%;"><center>Welcome to my Website</center></h1>
+<p1 style="background-color: ghostwhite;color:yellow;font-size: 250%;"><center>Make yourself at home</center></p1>
+<br><br><br><br>
+<img src="fbpic.jpg" alt="Me"style="width:400px;height:400px;" align="left" hspace="50" vspace="25">
+
+<h2 style="color: red;">Introduction:</h2>
+<p2 style="border: 2px rgba(100, 100, 100, 0.5); color: orange;font-family:FreeMono, monospace; font-size:160%; background-color: rgba(100, 100, 100, 0.5);"
+align= "justify">
+  Hello there! Kristian Cedric Y. Franco is my name. I am 20 years old and I live in La Carlota City's Gan Subdivision. I graduated from Doña Hortencia Salas Benedicto National High School (DHSBNHS) and La Carlota South Elementary School-1 (LCSES-1). My father's name is Jesus L. Franco, and my mother's name is Maria Purita Y. Franco. I have two brothers, JV Derick Y. Franco and Luis Jerick Y. Franco. My hobbies include playing video games, reading manga/manhwa/webtoon, eating, sleeping, visiting to new places, and watching anime. My ambition is to become a programmer while also living a pleasant life.
+<br><br><br><br></p2>
+<center><table style="color: white;">
+  <tr>
+    <th colspan="2"><center><b>Physical Attributes<b></center></th>
+  </tr>
+  <tr>
+    <td><b>Hair</b></td>
+    <td>Curly Black</td>
+  </tr>
+  <tr>
+    <td><b>Eyes</b></td>
+    <td>Narrow Eyes</td>
+  </tr>
+  <tr>
+    <td><b>Nose</b></td>
+    <td>Pango</td>
+  </tr>
+  <tr>
+    <td><b>Body Type</b></td>
+    <td>ABS (A Big Stomach)</td>
+  </tr>
+</table></center>
+<br><br><br><br><br><br><br><br>
+<p3 style="color: white;"><b>OBJECTIVE</b><br><i>"Ang nais ko ay maranasan ang umibig at masuklian din ng pag-ibig"</i></br></p3>
+<br><br>
+<p4 style="color: white;"><b>Citizenship</b><br><i>Filipino</i><br><br></p4>
+<p5 style="color: white;"><b>Date of birth:</b><br><i>22 January 2002</i><br><br></p5>
+<p6 style="color: white;"><b>Civil Status:</b><br><i>Single but taken(In short, Umaasa)</i><br><br></p6>
+<p7 style="color: white;"><b>Sex:</b><br><i>Hindi yan ang habol ko</i><br><br></p7>
+<p8 style="color: white;"><b>Weight:</b><br><i>75 kg. kapag busog</i><br><br></p8>
+<h3 style="color: red;"><b>SKILLS</b><br></h3>
+<ul style="color: white;">
+  <li>May takot sa Diyos</li>
+  <li>Mapagmahal sa pamilya</li>
+  <li>Marunong maghintay</li>
+  <li>Nagmahal kahit ang sakit sakit na</li>
+  <li>May panindigan</li>
+  <li>Never gonna give you up</li>
+  <li>Never gonna let you down</li>
+  <li>Never gonna run around and desert you</li>
+  <li>Never gonna make you cry</li>
+  <li>Never gonna say goodbye</li>
+  <li>Never gonna tell a lie and hurt you</li>
+</ul><br>
+<h4 style="color: red;"><b>HONORS/AWARDS</b><br></h4>
+<p9 style="color: white;"><b>Best Anak</b><br><i>Sabi ni Mama</i><br><br></p9>
+<p10 style="color: white;"><b>Sobrang Pogi</b><br><i>Sabi rin ni Mama</i><br><br></p10>
+<p11 style="color: white;"><b>HIV Negative</b><br><i>DOH, 2018</i><br><br></p11>
+<p12 style="color: white;"><b>#1 in the Most Handsome Faces of 2021</b><br><i>Youtube, 2021</i><br><br></p12>
+<h5 style="color: red;"><b>WORK EXPERIENCE</b><br></h5>
+<p13 style="color: white;"><b>Work, Work, Work, Work, Work, Work</b><br><i>Hard Worker sabi ni Rihanna</i><br><br></p13>
+<p14 style="color: white;"><b>Krusty Krab</b><br><i>Chef, 2018-2019</i><br><br></p14>
+<p15 style="color: white;"><b>Free Lunch Model</b><br><i>2017-2018</i><br><br></p15>
+<h6 style="color: red;"><b>CHARACTER REFERENCE</b><br></h6>
+<p16 style="color: white;"><i>"Ikaw Na!"</i><br><b>Boy Abunda</b><br><br></p16>
+<p17 style="color: white;"><i>"Gurl, sayo na tong korona!"</i><br><b>Pia Wurtzbach</b><br><br></p17>
+<p18 style="color: white;"><i>"Swerte mo, Girl!"</i><br><b>Liza Soberano</b><br><br></p18>
+<p19 style="color: white;"><i>"Salamat sa pagpaparaya sa kay Liza pre"</i><br><b>Enrique Gil</b><br><br></p19>
+<p20 style="color: white;"><i>"Dahil sayo ako'y matapang"</i><br><b>Inigo Pascual</b><br><br></p20>
+<p21 style="color: white;"><i>"Pasito a pasito, suave suavecito"</i><br><b>Luis Fonsi</b><br><br></p21>
+<p22 style="color: white;"><i>"Dubi-dubi dap-dap dubi-dubi dap-dap Dubi-dubi dap-dap dubi-dubi dip-dip
+Dubi-dubi dap-dap du-dap da-dap"</i><br><b>Willie Revillame</b><br><br><br><br></p22>
+
+<p23 style="color: white;font-size: 200%;"><i></i><br><b><center>Survey Lang!<br>Kakampink or Uniteam ka ba?</center></b><br></p23>
+
+<a href="https://www.youtube.com/watch?v=EEzxJuPkQnk"><center>
+<img src="image-removebg-preview (55).png" alt="Kakampink" style="width:200px;height:200px;"></center><br>
+</a>
+<a href="https://www.youtube.com/watch?v=yU32X5CLoTg" target="_blank"><center>
+<img src="image-removebg-preview (54).png" alt="Uniteam" style="width:200px;height:200px;"></center><br>
+</a>
+</body>
+</html>
